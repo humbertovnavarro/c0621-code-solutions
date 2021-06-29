768a7ae55b8a3bb81fe061946feeb0f8eb3abbe1
humberto.navarro.dev@gmail.com <humberto.navarro.dev@gmail.com>
2021-06-29 11:19:40 -0700

    Finished JavaScript data model

---
+++ javascript-data-model/item.js
@@ -0,0 +1,20 @@
+items: [
+  {
+    name: 'JavaScript for impatient programmers',
+    author: {
+      prefix: 'Dr.',
+      firstName: 'Axel',
+      lastName: 'Rauschmayer'
+    }
+      {
+    imgUrl: '',
+    price: 31.55,
+    support: false,
+    reviewed: false,
+    returnWindow: {
+      month: 10,
+      day: 7,
+      year: 2020
+    }
+  }
+]
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,165 @@
+var orderHistory = [
+  {
+    orderId: "114-3941689-8772232",
+    orderPlaced: {
+      month: 9,
+      day: 4,
+      year: 2020
+    },
+    orderDelivered: {
+      month: 9,
+      day: 8,
+      year: 2020
+    },
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        imgUrl: '',
+        price: 31.55,
+        support: false,
+        reviewed: false,
+        author: {
+          prefix: 'Dr.',
+          firstName: 'Axel',
+          lastName: 'Rauschmayer'
+        },
+        returnWindow: {
+          month: 10,
+          day: 7,
+          year: 2020
+        }
+      }
+    ],
+    total: 34.00,
+    shipTo:
+      {
+        firstName: 'JS',
+        lastName: 'Masher'
+      }
+  },
+  {
+    orderId: "113-9984268-1280257",
+    orderPlaced: {
+      month: 8,
+      day: 19,
+      year: 2020
+    },
+    orderDelivered: {
+      month: 8,
+      day: 20,
+      year: 2020
+    },
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        author: {
+          prefix: null,
+          firstName: 'Alexander',
+          lastName: 'Christopher'
+        },
+        imgUrl: '',
+        price: 41.33,
+        support: false,
+        reviewed: false,
+        returnWindow: {
+          month: 9,
+          day: 19,
+          year: 2020
+        }
+      }
+    ],
+    total: 44.53,
+    shipTo:
+    {
+      firstName: 'JS',
+      lastName: 'Masher'
+    }
+  },
+
+  {
+    orderId: "114-2875557-9059409",
+    orderPlaced: {
+      month: 8,
+      day: 4,
+      year: 2020
+    },
+    orderDelivered: {
+      month: 8,
+      day: 7,
+      year: 2020
+    },
+    items: [
+      {
+        name: 'Gamecube controller Adapter.',
+        author: null,
+        imgUrl: '',
+        price: 15.98,
+        support: false,
+        reviewed: false,
+        returnWindow: {
+          month: 9,
+          day: 5,
+          year: 2020
+        }
+      }
+    ],
+    total: 17.22,
+    shipTo:
+    {
+      firstName: 'JS',
+      lastName: 'Masher'
+    }
+  },
+
+  {
+    orderId: "113-2883177-2648248",
+    orderPlaced: {
+      month: 8,
+      day: 3,
+      year: 2020
+    },
+    orderDelivered: {
+      month: 8,
+      day: 5,
+      year: 2020
+    },
+    items: [
+      {
+        name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        imgUrl: '',
+        price: 94.45,
+        support: true,
+        reviewed: false,
+        returnWindow: {
+          month: 9,
+          day: 4,
+          year: 2020
+        }
+      },
+      {
+        name: 'The Art of Sql',
+        author: {
+          prefix: null,
+          firstName: 'Faroult',
+          lastName: 'Stephane'
+        },
+        imgUrl: '',
+        price: 33.99,
+        support: false,
+        reviewed: false,
+        returnWindow: {
+          month: 9,
+          day: 4,
+          year: 2020
+        }
+      }
+    ],
+    total: 139.93,
+    shipTo:
+    {
+      firstName: 'JS',
+      lastName: 'Masher'
+    }
+  }
+];
