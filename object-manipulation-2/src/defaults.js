4df07148c4279c6e2a01567cd27fde255b361334
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-06 10:31:29 -0700

    finished

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,4 +1,14 @@
 /* exported defaults */
-function defaults(target, source) {
+/*
+  for each key in source
+    if target at key is undefined
+      target at key equals source at key
+*/
 
+function defaults(target, source) {
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
 }
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,4 +1,16 @@
 /* exported omit */
+/*
+  set object equal to empty object
+  for key in source
+    set omit to false
+    set i to zero
+    while i is less than length of keys
+      if keys at i is strictly equal to key
+        set omit equal to true
+    if not omit
+      set object at key to source at key
+  return object
+*/
 function omit(source, keys) {
   var object = {};
   for (var key in source) {
