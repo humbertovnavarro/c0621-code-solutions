6b3b4bf7ad82950e162e9d867fdd6de7e06db43a
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-06 13:09:21 -0700

    fixed code styling

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,37 +1,35 @@
 /* exported isAnagram */
 
-//Compare the strings and return false if a disqualification is found
-function isAnagram(firstString,secondString){
+function isAnagram(firstString, secondString) {
   var stringOne = getOccurences(firstString);
   var stringTwo = getOccurences(secondString);
-  if(sameValues(stringOne,stringTwo)){
+  if (sameValues(stringOne, stringTwo)) {
     return true;
   }
   return false;
 }
 
-function sameValues(objectOne,objectTwo){
-  for(var key in objectOne){
-    if(objectTwo[key] === undefined){
+// Check if objects have the same values
+function sameValues(objectOne, objectTwo) {
+  for (var key in objectOne) {
+    if (objectTwo[key] === undefined) {
       return false;
     }
-    if(objectTwo[key] !== objectOne[key]){
+    if (objectTwo[key] !== objectOne[key]) {
       return false;
     }
   }
   return true;
 }
 
-
-//Create an object of the string with chars and the number of occurences sans space
-function getOccurences(string){
+// Create an object of the string with chars and the number of occurences sans space
+function getOccurences(string) {
   var occurences = {};
   for (var i = 0; i < string.length; i++) {
-    if(string[i] !== " "){
+    if (string[i] !== ' ') {
       if (occurences[string[i]] === undefined) {
         occurences[string[i]] = 1;
-      }
-      else {
+      } else {
         occurences[string[i]]++;
       }
     }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,13 +1,13 @@
 /* exported reverseWords */
-function reverseWords(string){
-  var reverse = "";
-  words = string.split(' ');
-  for(var i = 0; i < words.length; i++){
-    for(var x = words[i].length - 1; x >= 0; x--){
+function reverseWords(string) {
+  var reverse = '';
+  var words = string.split(' ');
+  for (var i = 0; i < words.length; i++) {
+    for (var x = words[i].length - 1; x >= 0; x--) {
       reverse += words[i][x];
     }
-    if(i !== words.length - 1){
-      reverse += " ";
+    if (i !== words.length - 1) {
+      reverse += ' ';
     }
   }
   return reverse;
