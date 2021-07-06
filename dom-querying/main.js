77e332ac7caccdc5a88f4fa97838afddcfe212f8
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-06 10:18:05 -0700

    finished

---
+++ dom-querying/main.js
@@ -0,0 +1,12 @@
+console.log('hello,world');
+var $header = document.querySelector('h1');
+console.log($header);
+console.dir($header);
+var $explanation = document.querySelector('#explanation');
+console.dir($explanation);
+var $hint = document.querySelector('.hint');
+console.dir($hint);
+var $paragraphs = document.querySelectorAll('p');
+console.log($paragraphs);
+var $links = document.querySelectorAll('.example-link');
+console.log($links);
