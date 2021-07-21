ced961a1a2643a9fd4dc9354c5603030487180f7
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-21 15:34:49 -0700

    Finished

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -10,9 +10,11 @@ var panSpeed = 3000;
 var index = 0;
 var $carousel = document.querySelector('.carousel');
 var $image = document.querySelector('.carousel img');
+generateDots();
 var $dots = document.querySelectorAll('.carousel .fa-circle');
 $dots[index].classList.replace('far', 'fas');
 $carousel.addEventListener('click', handleCarouselClick);
+
 var autoPan = setInterval(panImage, panSpeed);
 
 function handleCarouselClick(event) {
@@ -23,6 +25,10 @@ function handleCarouselClick(event) {
     panImage('right');
     resetTimer();
   }
+  if (event.target.getAttribute('data-control') === 'button') {
+    resetTimer();
+    setImage(event.target.getAttribute('data-index'));
+  }
 }
 
 function resetTimer() {
@@ -30,21 +36,42 @@ function resetTimer() {
   autoPan = setInterval(panImage, panSpeed);
 }
 
+function setImage(newIndex) {
+  var imageIndex = Number.parseInt(newIndex);
+  $image.src = 'images/' + images[imageIndex];
+  for (var i = 0; i < $dots.length; i++) {
+    $dots[i].classList.replace('fas', 'far');
+  }
+  $dots[imageIndex].classList.replace('far', 'fas');
+  index = imageIndex;
+}
+
+function generateDots() {
+  for (var i = 0; i < images.length; i++) {
+    // <i data-control="button" data-index="0" class="far fa-circle"></i>
+    var $buttonContainer = document.querySelector('.button-container');
+    var $dot = document.createElement('i');
+    $dot.className = 'far fa-circle';
+    $dot.setAttribute('data-control', 'button');
+    $dot.setAttribute('data-index', i);
+    $buttonContainer.appendChild($dot);
+  }
+}
+
 function panImage(direction) {
   $dots[index].classList.replace('fas', 'far');
   if (direction === 'left') {
-    if (index === 0) {
+    if (index <= 0) {
       index = images.length - 1;
     } else {
       index--;
     }
   } else {
-    if (index === images.length - 1) {
+    if (index >= images.length - 1) {
       index = 0;
     } else {
       index++;
     }
   }
-  $image.src = 'images/' + images[index];
-  $dots[index].classList.replace('far', 'fas');
+  setImage(index);
 }
