497c585635d531ed4c260913c8ddf618dbde4b16
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-08-12 12:30:13 -0700

    user can update a note

--- node-notes-cli/notebook.js
+++ node-notes-cli/notebook.js
@@ -15,6 +15,12 @@ class Entry {
     fs.writeFileSync('data.json', dataJSON, 'utf8', err => { if (err) throw err; });
   }
 
+  delete() {
+    delete data.notes[this.id];
+    const dataJSON = JSON.stringify(data, null, 2);
+    fs.writeFileSync('data.json', dataJSON, 'utf8', err => { if (err) throw err; });
+  }
+
   print() {
     console.log(`${this.id}: ${this.textContent}`);
   }
@@ -25,8 +31,8 @@ for (const key in notes) {
   entries.push(entry);
 }
 
-const [first, second] = args;
-if (first === 'read') {
+const [first, second, third] = args;
+if (first === 'read' || args.length === 0 || first === 'r') {
   if (!second) {
     readAll();
   } else {
@@ -39,15 +45,37 @@ if (first === 'read') {
       process.exit(1);
     }
   }
-} else if (first === 'add') {
+} else if (first === 'add' || first === 'a' || first === 'create') {
   if (!second) {
-    console.error('Please specify a title.');
+    console.error('Missing notes argument.');
+    process.exit(1);
   } else {
     const entry = new Entry(data.nextId, second);
     data.nextId++;
     entry.serialize();
     process.exit(0);
   }
+} else if (first === 'delete' || first === 'd' || first === 'remove') {
+  const entry = findId(second);
+  if (entry) {
+    entry.delete();
+    process.exit(0);
+  } else {
+    console.error('No such entry.');
+    process.exit(1);
+  }
+} else if (first === 'update' || first === 'u' || first === 'edit') {
+  if (!second) {
+    console.error('You need to specify an entry.');
+    process.exit(1);
+  }
+  if (!third) {
+    console.error('Please add new notes.');
+    process.exit(1);
+  }
+  const entry = findId(second);
+  entry.textContent = third;
+  entry.serialize();
 }
 
 function readAll() {
