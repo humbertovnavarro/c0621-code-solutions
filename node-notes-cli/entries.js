ee13230d730791936d82a603f4e9e6c8688b3580
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-08-12 16:55:22 -0700

    finished refactor to repository pattern

--- node-notes-cli/entries.js
+++ node-notes-cli/entries.js
@@ -1,6 +1,12 @@
 module.exports =
 class Entries {
   constructor(nextId, parsedNotes) {
+    this.operations = {
+      add: { regex: /^(a)$/, require: 1 },
+      replace: { regex: /^(u|update|replace|rep)$/, require: 2 },
+      delete: { regex: /^(rm|d|del|delete)$/, require: 1 },
+      print: { regex: /^(p|print|read|)$/, require: 0 }
+    };
     this.nextId = nextId;
     this.notes = parsedNotes;
     this.changes = false;
@@ -39,4 +45,56 @@ class Entries {
     return JSON.stringify(data, null, 2);
   }
 
+  getValidationError(command, args) {
+    const operation = this.operations[command];
+    const error = `${command} is not a valid operation.`;
+    if (!operation) {
+      return error;
+    }
+    if (args.length < operation.require) {
+      if (operation.require === 1) {
+        return `missing arguments, ${command} takes one argument`;
+      } else {
+        return `missings arguments, ${command} takes ${operation.require} arguments`;
+      }
+    } else if (args.length > operation.require) {
+      return 'too many arguments.';
+    } else {
+      return null;
+    }
+  }
+
+  interpretCommand(command) {
+    let output = command;
+    if (!command) {
+      return 'print';
+    }
+    for (const key in this.operations) {
+      output = output.replace(this.operations[key].regex, key);
+    }
+    return output;
+  }
+
+  process(command, args) {
+    const cmd = this.interpretCommand(command);
+    const error = this.getValidationError(cmd, args);
+    if (error) {
+      return error;
+    }
+    switch (cmd) {
+      case 'add':
+        this.add(args[0]);
+        break;
+      case 'delete':
+        this.deleteById(args[0]);
+        break;
+      case 'replace':
+        this.updateById(args[0], args[1]);
+        break;
+      case 'print':
+        this.readAll();
+        break;
+    }
+    return null;
+  }
 };
--- node-notes-cli/notebook.js
+++ node-notes-cli/notebook.js
@@ -1,92 +1,19 @@
-const args = process.argv.slice(2);
+const [, , command, ...args] = process.argv;
 const fs = require('fs');
 const data = require('./data.json');
-const entries = [];
-const notes = data.notes;
-class Entry {
-  constructor(id, textContent) {
-    this.id = id;
-    this.textContent = textContent;
-  }
-
-  serialize() {
-    data.notes[this.id] = this.textContent;
-    const dataJSON = JSON.stringify(data, null, 2);
-    fs.writeFileSync('data.json', dataJSON, 'utf8', err => { if (err) throw err; });
-  }
-
-  delete() {
-    delete data.notes[this.id];
-    const dataJSON = JSON.stringify(data, null, 2);
-    fs.writeFileSync('data.json', dataJSON, 'utf8', err => { if (err) throw err; });
-  }
-
-  print() {
-    console.log(`${this.id}: ${this.textContent}`);
-  }
+const Entries = require('./entries.js');
+const entries = new Entries(data.nextId, data.notes);
+const error = entries.process(command, args);
+if (error) {
+  console.error(error);
+  process.exit(2);
 }
-
-for (const key in notes) {
-  const entry = new Entry(key, notes[key]);
-  entries.push(entry);
-}
-
-const [first, second, third] = args;
-if (first === 'read' || args.length === 0 || first === 'r') {
-  if (!second) {
-    readAll();
-  } else {
-    const entry = findId(second);
-    if (entry) {
-      entry.print();
-      process.exit(0);
-    } else {
-      console.error('No such entry.');
+if (entries.changes) {
+  fs.writeFile('./data.json', entries.serialize(), error => {
+    if (error) {
+      console.error('Could not write to file. Check permissions?');
       process.exit(1);
     }
-  }
-} else if (first === 'add' || first === 'a' || first === 'create') {
-  if (!second) {
-    console.error('Missing notes argument.');
-    process.exit(1);
-  } else {
-    const entry = new Entry(data.nextId, second);
-    data.nextId++;
-    entry.serialize();
-    process.exit(0);
-  }
-} else if (first === 'delete' || first === 'd' || first === 'remove') {
-  const entry = findId(second);
-  if (entry) {
-    entry.delete();
-    process.exit(0);
-  } else {
-    console.error('No such entry.');
-    process.exit(1);
-  }
-} else if (first === 'update' || first === 'u' || first === 'edit') {
-  if (!second) {
-    console.error('You need to specify an entry.');
-    process.exit(1);
-  }
-  if (!third) {
-    console.error('Please add new notes.');
-    process.exit(1);
-  }
-  const entry = findId(second);
-  entry.textContent = third;
-  entry.serialize();
+  });
 }
-
-function readAll() {
-  entries.forEach(entry => entry.print());
-}
-
-function findId(id) {
-  for (const entry of entries) {
-    if (entry.id === id) {
-      return entry;
-    }
-  }
-  return null;
-}
+console.log(entries.serialize());
