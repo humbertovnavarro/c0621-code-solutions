1f16d559e717621ba572a23450d2eb53debb9d18
humbertovnavarro <kamaii199827@gmail.com>
2021-08-13 22:40:22 +0000

    done

---
+++ express-hello-world/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  console.log('Request method:', req.method);
+  res.send('Hi');
+  next();
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
