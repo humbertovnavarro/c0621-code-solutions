aa4ca856e254377807628113733f45d061c76fe9
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-08-20 11:20:49 -0700

    fixed filter function

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -1,7 +1,10 @@
 function filter(array, predicate) {
   const output = [];
   for (let i = 0; i < array.length; i++) {
-    output.push(predicate(array.length[i]));
+    const includes = predicate(array[i]);
+    if (includes) {
+      output.push(array[i]);
+    }
   }
   return output;
 }
