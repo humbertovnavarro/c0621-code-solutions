2695f122950cbdac6b40a41a1ad3c9b432b2a06a
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-08-23 17:00:52 -0700

    changed div to h1

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const $h1 = React.createElement('div', null, 'Hello, React!');
+const $h1 = React.createElement('h1', null, 'Hello, React!');
 ReactDOM.render($h1, document.getElementById('root'));
