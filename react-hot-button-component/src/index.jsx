d70276cb1a8424e0cbd5d3b873c7ba7e6ae20044
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-08-25 11:57:18 -0700

    moved color picking logic to render, refactor to not depend on state

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -3,32 +3,30 @@ import ReactDOM from 'react-dom';
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clicks: 1, className: 'hot-button frozen' };
+    this.state = { clicks: 1 };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    let className = this.state.className;
-    if (this.state.clicks === 6) {
-      className = 'hot-button cool';
-    }
-    if (this.state.clicks === 9) {
-      className = 'hot-button warm';
-    }
-    if (this.state.clicks === 12) {
-      className = 'hot-button hot';
-    }
-    if (this.state.clicks === 15) {
-      className = 'hot-button sweltering';
-    }
-    if (this.state.clicks === 18) {
-      className = 'hot-button nuclear';
-    }
-    this.setState({ clicks: this.state.clicks + 1, className: className });
+    this.setState({ clicks: this.state.clicks + 1 });
   }
 
   render() {
-    return <button onClick={this.handleClick} className={this.state.className}>Hot Button</button>;
+    let classString;
+    if (this.state.clicks <= 3) {
+      classString = 'hot-button frozen';
+    } else if (this.state.clicks <= 6) {
+      classString = 'hot-button cool';
+    } else if (this.state.clicks <= 9) {
+      classString = 'hot-button warm';
+    } else if (this.state.clicks <= 12) {
+      classString = 'hot-button hot';
+    } else if (this.state.clicks <= 15) {
+      classString = 'hot-button sweltering';
+    } else {
+      classString = 'hot-button nuclear';
+    }
+    return <button onClick={this.handleClick} className={classString}>Hot Button</button>;
   }
 }
 
